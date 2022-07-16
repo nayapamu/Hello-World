cdc6be4ef5cca6dcd19538906ae23d2e7ba3e075
nayapamu <surendranaya80@gmail.com>
2022-07-17 00:20:30 +0530

    first commit

---
+++ src/com/pratices/wellcomSuri.java
@@ -0,0 +1,15 @@
+package com.pratices;
+
+public class wellcomSuri {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		{
+			int a = 0;
+			if(a==0) {
+				System.out.println("a is zero");			}
+			
+		}
+	}
+
+}
